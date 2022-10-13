979216e4fd815c9728155df3a9a3cb3b105e6fee
vivekrai1999 <vivek.rai.2807@gmail.com>
2022-10-13 07:37:40 +0530

    HelloJava

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        // print function does not go to new line
+        // use \n for new line
+        System.out.print("Hello java \n");
+        // println function gor=es to new line
+        System.out.println("this is next line");
+        System.out.println("I am at new line");
+    }
+}
