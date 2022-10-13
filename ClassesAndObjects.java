6513b6cfc7a53b56d7c21cfaa03a25c396f771cf
vivekrai1999 <vivek.rai.2807@gmail.com>
2022-10-13 08:15:16 +0530

    creating objects

--- ClassesAndObjects.java
+++ ClassesAndObjects.java
@@ -1,4 +1,5 @@
-public class ClassesAndObjects {
+// creating a class in java
+class Box {
     // private variable can only be accessed inside the class
     private int length, breadth, height;
     // public functions can be called from outside the class by jvm
@@ -12,5 +13,20 @@ public class ClassesAndObjects {
         System.out.println("l: "+length);
         System.out.println("b: "+breadth);
         System.out.println("h: "+height);
+    }
+}
+
+// creating an object in java
+
+class ClassesAndObjects {
+    public static void main(String[] args){
+        // An object is made with the help of new keyword
+        Box smallBox = new Box();
+        smallBox.setDimension(12, 10, 5);
+        smallBox.showDimension();
+
+        // assigning new object to reference varibale smallbox
+        smallBox = new Box();
+        smallBox.showDimension();
     }
 }
