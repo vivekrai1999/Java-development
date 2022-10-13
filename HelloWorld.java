8bdebac49cb9e4372e0aaa3389022c15fae22e4e
vivekrai1999 <vivek.rai.2807@gmail.com>
2022-10-13 07:44:45 +0530

    Comments in java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,16 @@ public class HelloWorld{
         // println function gor=es to new line
         System.out.println("this is next line");
         System.out.println("I am at new line");
+
+        // Comments in java
+        // 1. Block Style:
+        /* this is a blockm style comment 
+        used dfot multi line comments */
+
+        // 2. Line style
+        // this is a line stle comment, terminated as soon as the line ends
+
+        //3. documentstion style
+        /** this style of comment is used for documnetation */
     }
 }
