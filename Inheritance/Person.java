acdeb3ae37dbc8e9d121567b0537e422f886823f
vivekrai1999 <vivek.rai.2807@gmail.com>
2022-10-15 06:21:20 +0530

    Inheritance

---
+++ Inheritance/Person.java
@@ -0,0 +1,19 @@
+package Inheritance;
+public class Person{
+    public String name;
+    public int age;
+    public void setName(String n){
+        name = n;
+    }
+    public void setAge(int a){
+        age = a;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+}
+
+
