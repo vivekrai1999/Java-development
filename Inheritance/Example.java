3ac26034a34b60746dc6f5f55a31329380950711
vivekrai1999 <vivek.rai.2807@gmail.com>
2022-10-15 06:20:35 +0530

    Inheritance

---
+++ Inheritance/Example.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Example {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("vivek");
+        s1.setAge(23);
+        s1.setRoll(2200930);
+        System.out.println(s1.getName());
+        System.out.println(s1.getAge());
+        System.out.println(s1.getRoll());
+    }
+}
---
+++ Inheritance/Student.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Student extends Person{
+    public int roll;
+    public void setRoll(int r){
+        roll =r;
+    }
+    public int getRoll(){
+        return roll;
+    }
+}  
